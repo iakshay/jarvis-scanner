7ab1c788ee0b9d1591c8676690145920ecc552f2
Akshay Aurora <akshayaurora@yahoo.com>
2019-04-21 17:41:15 -0400

    adding react-router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class JobSnippet extends Component {
   constructor(props) {
@@ -7,34 +7,164 @@ class JobSnippet extends Component {
   }
 
   render() {
-      return <div>
-      <div><strong> JobId </strong>{this.props.data.JobId}</div>
-      <div><strong> Type </strong>{this.props.data.Type}</div>
-      {this.props.data.Type === "PortScan" ? (
-        <div>
-        <div>{this.props.data.Data.Ip}</div>
-        <div>{this.props.data.Data.PortRange.Start}-{this.props.data.Data.PortRange.End}</div>
-        <div>{this.props.data.Data.Mode}</div>
-      </div>) : (<div>{this.props.data.Data.IpBlock}</div>)}
+      return (<div className="job-snippet row mb-10 p-3">
+      <div className="col-10">
+      <div><strong>#</strong>{this.props.data.JobId} / <strong>Type: </strong>{this.props.data.Type}</div>
+      {this.props.data.Type === "PortScan" ? 
+        (<div>Mode: {this.props.data.Data.Mode} / Ip: {this.props.data.Data.Ip} / Ports: {this.props.data.Data.PortRange.Start}-{this.props.data.Data.PortRange.End}</div>)
+        : (<div>Ip: {this.props.data.Data.IpBlock}</div>)}
+        </div>
+      <div className="col-2">
+        <Link className="btn btn-primary mr-1" to={`/view/${this.props.data.JobId}`}>detail</Link>
+        <button type="button" className="btn btn-danger mr-1">delete</button>
       </div>
+      </div>)
   }
 }
 
+class TaskView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (<div>{JSON.stringify(this.props.data)}</div>)
+  }
+}
+
+
 class JobSubmit extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jobType: "PortAlive"
+    }
+  }
+  handleChange = (e) => {
+        console.log(e.target.name, e.target.value);
+        this.setState({[e.target.name] : e.target.value});
+    };
   render() {
     return (
-      <div>
-      JobDetailView
+      <div className="p-3">
+      <h3>Submit job </h3>
+      <form>
+          <div className="row form-inline mb-3">
+           <div className="form-group col-2 mr-3">
+           <label for="jobType" className="col-sm-2">Type</label>
+            <div className="col-sm-4">
+            <select style={{width:'120px'}} className="custom-select"
+                    defaultValue="IsAlive"
+                    name="jobType" onChange={this.handleChange}>
+                <option value="IsAlive">IsAlive</option>
+                <option value="PortScan">PortScan</option>
+            </select>
+            </div>
+            </div>
+          <div className="form-group col-6 mr-2">
+          <label for="ipBlock" className="col-sm-2">IpBlock</label>
+          <div className="col-sm-4">
+          <input style={{width:'215px'}}className="form-control"
+                         type="text"
+                         name="ipBlock"
+                         placeholder="192.168.0.1"
+                         onChange={this.handleChange} />
+           </div>
+           </div>
+           </div>
+          {this.state.jobType === "PortScan" ?
+          (<div className="row form-inline mb-3">
+            <div className="form-group col-2 mr-3">
+            <label for="mode" className="col-sm-2">Mode</label>
+             <div className="col-sm-4">
+            <select style={{width:'120px'}} className="custom-select"
+                    defaultValue="NormalScan"
+                    name="mode" onChange={this.handleChange}>
+                <option value="NormalScan">Normal</option>
+                <option value="SynScan">SYN</option>
+                <option value="FinScan">FIN</option>
+            </select>
+            </div>
+            </div>
+            <div className="form-group col-6 mr-3">
+            <label for="portMin" className="col-sm-2">Ports</label>
+            <div className="col-sm-6">
+            <input style={{width:'100px'}} className="form-control "
+                           type="number"
+                           name="portMin"
+                           defaultValue="0"
+                           onChange={this.handleChange} />
+                           <span> - </span>
+            <input style={{width:'100px'}} className="form-control"
+                           type="number"
+                           name="portMax"
+                           defaultValue="65535"
+                           onChange={this.handleChange} /></div></div></div>) : '' }
+
+          <button type="button" className="btn btn-primary">submit</button>
+      </form>
       </div>
     );
   }
 }
 
 class JobDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: {}};
+  }
+
+  componentDidMount() {
+    console.log('foo');
+    // fetch jobs detail here
+    this.setState({'data':
+    {
+      JobId: 1,
+      Data: [
+      {
+        WorkerId: 1,
+        WorkerName: "Worker1",
+        Data: {
+          Ip: "192.168.2.1",
+          Status: 0
+        }
+      },
+      {
+        WorkerId: 2,
+        WorkerName: "Worker2",
+        Data: {
+          Ip: "192.168.2.1",
+          Status: 0,
+        },
+      },
+      {
+        WorkerId: 3,
+        WorkerName: "Worker3",
+        Data: {
+          Ip: "192.168.2.1",
+          Status: 0,
+        },
+      },
+      {
+        WorkerId: 4,
+        WorkerName: "Worker4",
+        Data: {
+          Ip: "192.168.2.1",
+          Status: 0,
+        }
+      }]
+    }});
+  }
+  
   render() {
+    console.log(this.state);
     return (
       <div>
-      JobDetailView
+      JobDetailView {this.props.match.params.id}
+      <div>
+          {this.state.data.Data && this.state.data.Data.map(task => (<TaskView data={task} />))}
+      </div>
       </div>
     );
   }
@@ -48,7 +178,6 @@ class JobList extends Component {
   
   componentDidMount() {
     // fetch jobs here
-    console.log('foo');
     this.setState({'data':
     [{
       JobId: 1,
@@ -78,20 +207,43 @@ class JobList extends Component {
   render() {
     return (
       <div>
-      <h1>ListView</h1>
-      <ul>
           {this.state.data.map(job => (<JobSnippet data={job} />))}
-      </ul>
       </div>
     );
   }
+}
+
+function Home() {
+  return (<div><JobSubmit /><JobList /></div>)
+}
+
+function About() {
+  return <h3>About</h3>;
 }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <JobList />
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <span className="navbar-brand mb-0 h1">Jarvis Scanner</span>
+            <ul className="navbar-nav mr-auto">
+             <li className="nav-item active">
+                <Link className="nav-link active" to='/'>Home</Link>
+             </li>
+            <li className="nav-item">
+                <Link className="nav-link" to='/about'>About</Link>
+            </li>
+                
+            </ul>
+        </nav>
+        <div className="container">
+          <Route path="/" exact component={Home} />
+          <Route path="/about/" component={About} />
+          <Route path='/view/:id' component={JobDetail} />
+        </div>
+      </Router>
       </div>
     );
   }
